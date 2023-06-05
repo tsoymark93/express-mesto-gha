1ef6900ae2050e8ad9a68456d7cadaa796fb9645
Mark Tsoy <tsoy.mark@MacBook-Air-Mark.local>
2023-06-05 15:02:52 +0600

    git hub tests 9

--- controllers/cards.js
+++ controllers/cards.js
@@ -34,6 +34,8 @@ module.exports.deleteCard = (req, res) => {
     .catch((err) => {
       if (err && err.name && err.name === 'TypeError') {
         res.status(404).send({ message: 'Запрашиваемая карточка не найдена' });
+      } else if (err && err.name && err.name === 'CastError') {
+        res.status(404).send({ message: 'Запрашиваемая карточка не найдена' });
       } else {
         res.status(500).send({ message: 'Ошибка сервера' });
       }
